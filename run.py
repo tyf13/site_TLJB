a02ca3a20bfb9a508bbcba3326b4501744b57853
tyf13 <tyfenn.boulanger@isen.yncrea.fr>
2025-07-03 16:07:51 +0200

    Add files via upload

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -6,5 +7,7 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+port = int(os.environ.get("PORT", 5000))
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
