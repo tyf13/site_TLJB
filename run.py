7931ea4c5b3668835236e51ec4e1ecc2e5801e65
tyf13 <tyfenn.boulanger@isen.yncrea.fr>
2025-07-03 17:11:00 +0200

    Add files via upload

--- run.py
+++ run.py
@@ -6,7 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/projet')
+def projet():
+    return render_template('projet.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
