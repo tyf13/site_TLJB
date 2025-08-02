19acad63a33a19e67930ee145e6a16e9d5a037b6
tyf13 <tyfenn.boulanger@isen.yncrea.fr>
2025-08-02 18:16:08 +0200

    Update run.py

--- run.py
+++ run.py
@@ -15,5 +15,5 @@ def contact():
     return render_template('contact.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))
     app.run(host='0.0.0.0', port=port)
